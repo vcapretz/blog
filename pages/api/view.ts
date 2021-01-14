71a830c5fbc783f4ecce7894f5332e1002b61415
Vitor Capretz <capretzvitor@gmail.com>
2021-01-14 01:58:30 +0100

    trigger deploy

--- pages/api/view.ts
+++ pages/api/view.ts
@@ -13,7 +13,9 @@ export default async function view(req: NextApiRequest, res: NextApiResponse) {
 
   const { snapshot } = await ref.transaction((currentViews: number) => {
     // if it has never been set it returns null
-    if (currentViews === null) currentViews = 0;
+    if (currentViews === null) {
+      currentViews = 0;
+    }
     return currentViews + 1;
   });
 
